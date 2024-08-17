30516a43e0a2a088a56a056dc0fc3e517ba3b699
Erick980 <erickgamesreis@gmail.com>
2024-08-16 22:19:32 -0300

    Create Products

--- App.js
+++ App.js
@@ -1,81 +1,26 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  StyleSheet,
-  Image,
-  Text,
-  StatusBar,
-} from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-import {List} from 'react-native-paper'
-
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Tropa da Taxa',
-    description: 'Era uma vez um cidadão de bem, que acabou encontrando o TAXAD'
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Até a última Taxa',
-    description: 'Era uma vez um cidadão de bem, que acabou encontrando o TAXAD'
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Homem da Taxa',
-    description: 'Era uma vez um cidadão de bem, que acabou encontrando o TAXAD'
-  },
-  {
-    id: '1',
-    title: 'Como sonegar imposto',
-    description: 'Era uma vez um cidadão de bem, que acabou encontrando o TAXAD'
-  },
-];
-
-// Leitura do title
-const Item = ({elemento: item}) => {
-  return(
-  <List.Item
-    title= {item.title}
-    description= {item.description}
-    // left={props => <List.Icon {...props} icon="folder" />}
-    left={props => <Image {...props} style={styles.tinyLogo} source={{uri: 'https://i.pinimg.com/originals/f1/f4/ea/f1f4eabb2e148d9f38bf23c93b84ca4f.jpg'}}/>}
-  />
-)};
-
-const App = () => {
+function HomeScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item elemento={item} />} // Renderiza o componente
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
---
+++ data/Produtos.js
@@ -0,0 +1,21 @@
+export default [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'Mouse',
+      description: 'Descrição do Mouse Golden Bullion',
+      urlImagem: 'https://i.pinimg.com/736x/25/24/96/252496a254a40a20343004864c22f5ef.jpg'
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Zeca Urubu',
+      description: 'Descrição do Zeca Urubu',
+      urlImagem: 'https://http2.mlstatic.com/D_NQ_NP_664887-MLB70586852543_072023-O.webp'
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Bicicleta 3x4',
+      description: 'Descrição da bicicleta 3x4',
+      urlImagem: 'https://img.freepik.com/fotos-gratis/bicicleta-legal-em-estudio_23-2150884292.jpg'
+    }
+  
+  ];
